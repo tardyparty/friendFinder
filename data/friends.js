6374dae15f99247673ffde20d3fadab48c783f6c
tardyparty <tardyparty@users.noreply.github.com>
2019-10-22 22:51:22 -0500

    figuring out modal content

--- data/friends.js
+++ data/friends.js
@@ -1,18 +1,18 @@
 var friendsArray = [
     {
-        name: "David",
-        photo: "www.google.com",
+        name: "Garrett Wise",
+        photo: "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
         scores: [
-            1,
+            3,
             2,
             3,
             4,
             5,
-            1,
-            2,
-            3,
             4,
-            5
+            2,
+            2,
+            4,
+            1
         ]
     }
 ];
